f273210bc15dc5dc222666d5ededa9bff48fa3de
maximkrouk <id.maximkrouk@gmail.com>
2026-09-03 11:18:23 +0200

    chore: record latest dependency tags

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/capturecontext/swift-keypaths-extensions.git",
-			.upToNextMinor(from: "0.2.0")
+			.upToNextMinor(from: "0.2.1")
 		),
 		.package(
 			url: "https://github.com/pointfreeco/swift-custom-dump.git",
